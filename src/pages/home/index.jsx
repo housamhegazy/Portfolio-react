7a91b3803a9a0f73b29209e5268ebbf48696b769
Housam Hegazy <geohousam@gmail.com>
2023-05-07 09:06:18 +0000

    update

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,18 @@
+import { Stack, Typography } from "@mui/material";
 
-
-
-export default function Home(){
-    return (
-        <h1>home</h1>
-    )
+export default function Home() {
+  return (
+    <Stack>
+      <Typography variant="body1">
+        Hi there, My name is Housam Hegazy. I’m a Self-motivated Front-end Web
+        Developer with a strong ambition to learn and discover the newest
+        cutting-edge methodologies to build high-performance websites. Capable
+        of self-managing in addition to collaborating with others in group
+        projects. Skilled in using HTML& HTML5, CSS& CSS3, BootStrap,
+        Javascript, React, MUI, Firebase, and Redux. Excellent communication
+        skills with clients to establish the best service quality to accomplish
+        customer satisfaction
+      </Typography>
+    </Stack>
+  );
 }
