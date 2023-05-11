d79b36c3cf6a9eb86102d27438f4923b55c94509
Housam Hegazy <geohousam@gmail.com>
2023-05-11 10:33:58 +0000

    update

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,26 +1,54 @@
-import React ,{useMemo} from 'react'
-import { useLoadScript,GoogleMap ,Marker } from '@react-google-maps/api'
+// import React, { useMemo } from "react";
+// import { useLoadScript, GoogleMap, Marker } from "@react-google-maps/api";
+import { Box, useTheme } from "@mui/material";
+
 export default function About() {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
-  });
-   const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
+  const theme = useTheme();
+  // const { isLoaded } = useLoadScript({
+  //   googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+  // });
+  //  const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
   return (
-    <div style={{width:'80vw',height:"90vh",margin:"10px"}}>
-    {!isLoaded ? (
-      <h1>Loading...</h1>
-    ) : (
-      <GoogleMap
-        mapContainerClassName="map-container"
-        center={center}
-        zoom={10}
-      >
-      <Marker
-  position={{ lat: 18.52043, lng: 73.856743 }}
-  icon={"http://maps.google.com/mapfiles/ms/icons/green-dot.png"}
-/>
-      </GoogleMap>
-    )}
-  </div>
-  )
+    <Box
+      sx={{
+        p: "20px",
+        background: theme.palette.primary.main,
+        width: { xs: "90%", md: "70%" },
+        mx: "auto",
+        borderRadius: "10px",
+        marginTop: "30px",
+      }}
+    >
+      <ul style={{ listStyle: "none" }}>
+        <li style={{ marginTop: "15px",color:"white" }}>
+          <span style={{color:"yellow"}}>2015</span>{" - "} Graduated from Faculity of Science
+        </li>
+        <li style={{ marginTop: "15px",color:"white" }}>
+          <span style={{color:"yellow"}}>2016-2018</span>{" - "}Worked as a data entry employee and government
+          services
+        </li>
+        <li style={{ marginTop: "15px",color:"white" }}>
+          <span style={{color:"yellow"}}>2018-now</span>{" - "}Worked as Nutrition Specialist at Dr.Nutrition
+          Center in Saudi Arabia
+        </li>
+        
+      </ul>
+    </Box>
+    //     <div style={{width:'80vw',height:"90vh",margin:"10px"}}>
+    //     {!isLoaded ? (
+    //       <h1>Loading...</h1>
+    //     ) : (
+    //       <GoogleMap
+    //         mapContainerClassName="map-container"
+    //         center={center}
+    //         zoom={10}
+    //       >
+    //       <Marker
+    //   position={{ lat: 18.52043, lng: 73.856743 }}
+    //   icon={"http://maps.google.com/mapfiles/ms/icons/green-dot.png"}
+    // />
+    //       </GoogleMap>
+    //     )}
+    //   </div>
+  );
 }
